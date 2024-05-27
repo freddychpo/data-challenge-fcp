bc5b9f03054cdbbdc27b95f25c971c4b2acfac0d
Freddy Portella <freddychpo@gmail.com>
2024-05-26 21:36:41 -0500

    updating mysql script

--- scripts/data_challenge_db.sql
+++ scripts/data_challenge_db.sql
@@ -5,26 +5,20 @@ use data_challenge_db;
 
 create table department (
   id int primary key auto_increment,
-  name varchar(255) not null
+  name varchar(50) not null
 );
 
 create table job (
   id int primary key auto_increment,
-  name varchar(255) not null
+  name varchar(50) not null
 );
 
 create table employee (
   id int primary key auto_increment,
-  name varchar(255) not null
-);
-
-create table hired_employees (
-  id int primary key auto_increment,
-  employee_id int,
+  name varchar(50) not null,
   hire_datetime datetime not null,
   department_id int,
   job_id int,
-  foreign key (employee_id) references employee(id),
   foreign key (department_id) references department(id),
   foreign key (job_id) references job(id)
 );
