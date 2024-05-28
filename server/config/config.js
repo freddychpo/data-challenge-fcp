bde58afffe7dc532743cebcf46df5c7e519c1302
Freddy Portella <freddychpo@gmail.com>
2024-05-28 12:16:29 -0500

    config.js dev local version

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 
-const clearDBUrl = process.env.CLEARDB_DATABASE_URL || '';
-
-const dbUrl = new URL(clearDBUrl);
-
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    host: dbUrl.hostname || process.env.DB_HOST,
-    user: dbUrl.username || process.env.DB_USER,
-    password: dbUrl.password || process.env.DB_PASSWORD,
-    database: dbUrl.pathname.replace('/', '') || process.env.DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   }
 };
