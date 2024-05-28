bab7afe19c7fe2d516c8ec286cafba1586ad68d7
Freddy Portella <freddychpo@gmail.com>
2024-05-28 17:26:10 -0500

    addind automates tests with mocha, chai & supertest

--- test/api.test.mjs
+++ test/api.test.mjs
@@ -1,14 +1,15 @@
-const request = require('supertest');
-const chai = require('chai');
-const app = require('../server');
-const path = require('path');
-const fs = require('fs');
+import * as chai from 'chai';
+import app from '../server.js';
+import path from 'path';
+import fs from 'fs';
+import supertest from 'supertest';
 
+const request = supertest(app);
 const expect = chai.expect;
 
 describe('API endpoints', () => {
     it('should upload a valid CSV file', done => {
-      request(app)
+      request
         .post('/upload-csv')
         .attach('file', fs.readFileSync(path.join(__dirname, 'valid.csv')), 'valid.csv')
         .end((err, res) => {
@@ -18,21 +19,19 @@ describe('API endpoints', () => {
     });
 
     it('should get all employees hired by quarter in 2021', done => {
-      request(app)
+      request
         .get('/api/employees/employees-hired-quarters')
         .end((err, res) => {
           expect(res.statusCode).to.equal(200);
-          //  Here we can add more assertions based on the structure of our data
           done();
         });
     });
 
     it('should get all departments with above mean hires in 2021', done => {
-      request(app)
+      request
         .get('/api/departments/departments-above-mean')
         .end((err, res) => {
           expect(res.statusCode).to.equal(200);
-          //  Here we can add more assertions based on the structure of our data
           done();
         });
     });
