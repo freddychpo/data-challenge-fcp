4de1277bcb2750b712d9c3923fb867f88bd1c073
Freddy Portella <freddychpo@gmail.com>
2024-05-28 11:58:09 -0500

    ClearDB Environment variables & db configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 
+const clearDBUrl = process.env.CLEARDB_DATABASE_URL || '';
+
+const dbUrl = new URL(clearDBUrl);
+
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: dbUrl.hostname || process.env.DB_HOST,
+    user: dbUrl.username || process.env.DB_USER,
+    password: dbUrl.password || process.env.DB_PASSWORD,
+    database: dbUrl.pathname.replace('/', '') || process.env.DB_NAME
   }
 };
