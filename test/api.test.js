bdc7a8a6e2668dc65ddfcff4503945cd5c9871b2
Freddy Portella <freddychpo@gmail.com>
2024-05-28 16:45:16 -0500

    fixing curly brackets issue

--- test/api.test.js
+++ test/api.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const chai = require('chai');
 const app = require('../server');
 const path = require('path');
+const fs = require('fs');
 
 const expect = chai.expect;
 
@@ -16,24 +17,23 @@ describe('API endpoints', () => {
         });
     });
 
-describe('API endpoints', () => {
-  it('should get all employees hired by quarter in 2021', done => {
-    request(app)
-      .get('/api/employees/employees-hired-quarters')
-      .end((err, res) => {
-        expect(res.statusCode).to.equal(200);
-        //  Here we can add more assertions based on the structure of our data
-        done();
-      });
-  });
+    it('should get all employees hired by quarter in 2021', done => {
+      request(app)
+        .get('/api/employees/employees-hired-quarters')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          //  Here we can add more assertions based on the structure of our data
+          done();
+        });
+    });
 
-  it('should get all departments with above mean hires in 2021', done => {
-    request(app)
-      .get('/api/departments/departments-above-mean')
-      .end((err, res) => {
-        expect(res.statusCode).to.equal(200);
-        //  Here we can add more assertions based on the structure of our data
-        done();
-      });
-  });
+    it('should get all departments with above mean hires in 2021', done => {
+      request(app)
+        .get('/api/departments/departments-above-mean')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          //  Here we can add more assertions based on the structure of our data
+          done();
+        });
+    });
 });
