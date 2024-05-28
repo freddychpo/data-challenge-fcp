b872886d54c93f2b2cfe56ec939e8ad76d84ceb1
Freddy Portella <freddychpo@gmail.com>
2024-05-28 16:26:31 -0500

    adding code template for automated tests

---
+++ test/api.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const chai = require('chai');
+const app = require('../server'); // Importa tu app de Express
+
+const expect = chai.expect;
+
+describe('API endpoints', () => {
+  it('should get all employees hired by quarter in 2021', done => {
+    request(app)
+      .get('/api/employees/employees-hired-quarters')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        //  Here we can add more assertions based on the structure of your data
+        done();
+      });
+  });
+
+  it('should get all departments with above mean hires in 2021', done => {
+    request(app)
+      .get('/api/departments/departments-above-mean')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        //  Here we can add more assertions based on the structure of your data
+        done();
+      });
+  });
+});
